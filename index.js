1ec439982b2c809b50318dfdb57a0bec48456a93
Suksit Sripitchayaphan <suksit@gmail.com>
2017-11-29 19:11:13 +0700

    add more info to notification

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const FeedParser = require('feedparser')
 const request = require('request')
 const moment = require('moment')
 const fs = require('fs')
+const cheerio = require('cheerio')
+const axios = require('axios')
 
 const config = require('./config.json')
 
@@ -37,31 +39,41 @@ parser.on('readable', function () {
     const lastUpdate = moment(config.lastUpdate)
 
     if (pubdate.isAfter(lastUpdate)) {
-      config.tokens.forEach(function (token) {
-        const title = item.title.replace('Udemy - ', '')
+      let $ = cheerio.load(item['atom:content']['#'])
+      const UDEMY_URL = $('.take-btn a').attr('href').split('?')[0]
 
-        const link = item['atom:link'].find(function (link) {
-          return link['@'].rel === 'alternate'
-        })['@'].href
+      axios.get(UDEMY_URL).then(function (res) {
+        $ = cheerio.load(res.data)
+        const rating = $('.rate-count .tooltip-container span').attr('aria-label').replace(' out of ', '/').replace('.0', '')
+        const reviews = $('.rate-count .tooltip-container .tooltip')[0].prev.data.replace('\n', '').trim()
+        const students = $('[data-purpose=enrollment]').first().text().replace('\n', '').replace(' enrolled', '').trim()
+        const published = $('[data-purpose=last-update-date]').text().replace('ed', 'ed:').replace('\n', '').trim()
 
-        const postData = {
-          url: NOTIFY_URL,
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            'Authorization': `Bearer ${token.value}`
-          },
-          formData: {
-            message: `${title}\r\n${link}`
+        config.tokens.forEach(function (token) {
+          const title = item.title.replace('Udemy - ', '')
+          const link = item['atom:link'].find(function (link) {
+            return link['@'].rel === 'alternate'
+          })['@'].href
+
+          const postData = {
+            url: NOTIFY_URL,
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              'Authorization': `Bearer ${token.value}`
+            },
+            formData: {
+              message: `${title}\r\n${rating} ${reviews}\r\nPopularity: ${students}\r\n${published}\r\n${link}`
+            }
           }
-        }
 
-        console.log(`sending notification to ${token.name}...`)
+          console.log(`sending notification to ${token.name}...`)
 
-        request.post(postData, (err, res) => {
-          if (err) {
-            console.error(err)
-          }
-          console.log(res.body)
+          request.post(postData, (err, res) => {
+            if (err) {
+              console.error(err)
+            }
+            console.log(res.body)
+          })
         })
       })
     }
